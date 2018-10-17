bda6591bbba0391bb665871a321c65c25ae1159b
Renato Marinho Alves <renato.mar.alves@gmail.com>
2018-10-16 21:41:42 -0300

    Alteração no menu superior e no footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BarraTopoComponent } from './barra-topo/barra-topo.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BarraTopoComponent
+    BarraTopoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
