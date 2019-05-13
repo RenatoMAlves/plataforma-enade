f4f42446bb0c202caeb09ec75e7b5ad15b00967e
Alexandre Moraes Matos <alexandremt03@gmail.com>
2019-05-13 00:22:44 -0300

    Teste de gráficos para resultados de associação

--- src/app/associacao/associacao/associacao.component.ts
+++ src/app/associacao/associacao/associacao.component.ts
@@ -6,7 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./associacao.component.scss']
 })
 export class AssociacaoComponent implements OnInit {
-
+  public teste: any[] = [
+    {
+      "name": "Sudeste - 4",
+      "value": 5368
+    },
+    {
+      "name": "Universidade - 4",
+      "value": 7053
+    },
+    {
+      "name": "Privada - 4",
+      "value": 7871
+    },
+    {
+      "name": "Sudeste,Universidade - 4",
+      "value": 4359
+    },
+    {
+      "name": "Privada,Sudeste - 4",
+      "value": 4144
+    },
+    {
+      "name": "Privada,Universidade - 4",
+      "value": 4516
+    },
+    {
+      "name": "Privada,Sudeste,Universidade",
+      "value": 3208
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
